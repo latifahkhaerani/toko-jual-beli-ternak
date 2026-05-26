42b2f752e2cc950f1f9f843d9b1bcb7500be130a
malthafkiram <malthafkiram@gmail.com>
2026-05-26 16:17:35 +0700

    config

---
+++ migrations/20260526091619-create-livestock.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Livestock', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Livestock');
+  }
+};
---
+++ models/transaction.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Transaction.init({
+    UserId: DataTypes.INTEGER,
+    LivestockId: DataTypes.INTEGER,
+    totalPaid: DataTypes.INTEGER,
+    receiptNumber: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+  });
+  return Transaction;
+};
